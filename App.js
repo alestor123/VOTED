a8064469ea63c8d269b3e0a32138bf53194210f4
alestor123 <alestoraldous@gmail.com>
2021-02-03 16:38:26 +0530

    added model

--- App.js
+++ App.js
@@ -4,3 +4,11 @@ axios = require('axios'),
 mongoose = require('mongoose'),
 port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/voted', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+app.get('/vote/:user/:repos/:option', async (req, res) => {
+var { user, repos,option } = req.params,
+votef = 1,
+{ counter } = await Model.findOneAndUpdate({ key: `${user}/${repo}` }, { $inc: { counter: flag } }, { upsert: true, new: true }).exec()
+
+
+})
+app.listen(port, () => console.log(`Server running at ${port}`))
