cbb1b233c83bb1601e44e9dd062fe63b21ec470e
alestor123 <alestoraldous@gmail.com>
2021-02-03 16:30:58 +0530

    mongoose connect

--- App.js
+++ App.js
@@ -1,1 +1,6 @@
 require('dotenv').config()
+var app = require('express')(),
+axios = require('axios'),
+mongoose = require('mongoose'),
+port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/voted', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
